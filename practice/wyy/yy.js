c0a08c2a0f66227b207b40d01cfaf0c91b1d5e18
klous <klous530.outlook.com>
2017-08-28 18:50:02 +0800

    New files

--- practice/wyy/yy.js
+++ practice/wyy/yy.js
@@ -9,14 +9,14 @@ $("#login-b, #login").mouseover(function(){
 });
 
 $(".top-nav li").mouseover(function(){
-  sy = $(this).index(".top-nav li")
+  sy = $(this).index()
   $(".ss .top1").eq(sy).show()
   $(".top-nav ul li div").eq(sy).show()
   $(".ss .top1").not($(".ss .top1").eq(sy)).hide()
   $(".top-nav ul li div").not($(".top-nav ul li div").eq(sy)).hide()
 
   $(".top-nav li").mouseout(function(){
-    sy = $(this).index(".top-nav li")
+    sy = $(this).index()
     $(".ss .top1").eq(sy).hide()
     $(".top-nav ul li div").eq(sy).hide()
   });
